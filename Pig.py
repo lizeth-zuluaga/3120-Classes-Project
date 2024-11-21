9961acd656e1e17da1c776d6477850db3f290dd2
lizeth-zuluaga <liz.alz@hotmail.com>
2024-11-21 18:01:19 -0500

    Add files via upload

---
+++ Pig.py
@@ -0,0 +1,14 @@
+//Lizeth Zuluaga
+
+class Pig(Animal):
+    def __init__(self, name):
+        super().__init__(name, species="pig")
+    def speak(self):
+        print("hoy,hoy,hoy!")
+        
+def main():
+    pig =Pig("Lola")
+    pig.display_info()
+    print(pig.speak())
+if __name__ == "__main__":
+        main()
