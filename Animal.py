996439cf0e7efcab7a62a80f28addd762dfcc206
Vlad Dzerzhynskyi <86209562+dzerzhynskyi@users.noreply.github.com>
2024-11-24 20:56:34 -0500

    Animal.py

--- Animal.py
+++ Animal.py
@@ -17,62 +17,20 @@ class Animal:
         print(f"Species: {self.species}")
 
 
-# In[2]:
-
-
-class Dog(Animal):
+class Chicken(Animal):
     def __init__(self, name):
-        super().__init__(name, species="Dog")
+        super().__init__(name, species="birds")
 
-
-# In[3]:
-
-
-class Cat(Animal):
-    def __init__(self, name):
-        super().__init__(name, species="Cat")
-
-
-# In[4]:
-
-
-class Parrot(Animal):
-    def __init__(self, name):
-        super().__init__(name, species="Parrot")
-
-
-# In[13]:
-
-
-class Pig(Animal):
-    def __init__(self, name):
-        super().__init__(name, species="pig")
-
-
-# In[14]:
-
+    def speak(self):
+        print("Chick!")
 
 def main():
-    dog = Dog("Bella")
-    cat = Cat("Kitty")
-    parrot = Parrot("Paco")
-    pig = Pig("Lola")
-    
-    dog.display_info()
-    print() 
-    cat.display_info()
-    print()  
-    parrot.display_info()
-    print()
-    pig.display_info()
-
-
-# In[15]:
-
+    chicken = Chicken("Lola")
+    chicken.display_info()
+    chicken.speak()
 
 if __name__ == "__main__":
-    main() 
-
+    main()
 
 # In[ ]:
 
